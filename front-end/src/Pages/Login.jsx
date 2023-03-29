747a237d457fa8577f0bd07ed505fe79ebd9793a
PedroSantana <phsantana99@gmail.com>
2023-03-29 19:39:51 -0300

    feat: requisito 3 finalizado

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [formInput, setFormInput] = useState({ email: '', password: '' });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const inputHandler = ({ target: { name, value } }) => {
+    setFormInput({ ...formInput, [name]: value });
+  };
+
+  const MIN_LENGTH_PASSWORD = 5;
+
+  useEffect(() => {
+    const validator = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    const validInput = formInput.email && formInput.password
+      && (validator.test(formInput.email))
+      && formInput.password.length > MIN_LENGTH_PASSWORD;
+
+    setIsDisabled(!validInput);
+  }, [formInput]);
+
   return (
     <div>
       <h1>Login</h1>
@@ -12,6 +31,9 @@ function Login() {
             type="email"
             id="email-input"
             data-testid="common_login__input-email"
+            name="email"
+            required
+            onChange={ inputHandler }
           />
         </label>
 
@@ -21,12 +43,16 @@ function Login() {
             type="password"
             id="password-input"
             data-testid="common_login__input-password"
+            name="password"
+            required
+            onChange={ inputHandler }
           />
         </label>
 
         <button
           type="submit"
           data-testid="common_login__button-login"
+          disabled={ isDisabled }
         >
           Login
         </button>
