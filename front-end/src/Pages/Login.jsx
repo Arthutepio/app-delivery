f736ff49218fad452c7113a4b626d0fd6a9381bd
PedroSantana <phsantana99@gmail.com>
2023-03-29 19:26:56 -0300

    feat: requisito 2 finalizado

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -3,7 +3,48 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      <p>Deu bom</p>
+      <h1>Login</h1>
+
+      <form action="">
+        <label htmlFor="email-input">
+          <span>Login</span>
+          <input
+            type="email"
+            id="email-input"
+            data-testid="common_login__input-email"
+          />
+        </label>
+
+        <label htmlFor="password-input">
+          <span>Password</span>
+          <input
+            type="password"
+            id="password-input"
+            data-testid="common_login__input-password"
+          />
+        </label>
+
+        <button
+          type="submit"
+          data-testid="common_login__button-login"
+        >
+          Login
+        </button>
+
+        <button
+          type="button"
+          data-testid="common_login__button-register"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
+
+      <span
+        data-testid="common_login__element-invalid-email"
+      >
+        Erro
+      </span>
+
     </div>
   );
 }
