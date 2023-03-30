71f9d6d78eb1e5764b99846a34c6e2d1dd3506b5
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-03-30 16:32:58 -0300

    feat: Adicionado validações de cadastro

--- front-end/src/Pages/Register.jsx
+++ front-end/src/Pages/Register.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Register() {
+  const [formInput, setFormInput] = useState({ email: '', password: '', name: '' });
+  const [isDisabled, setIsDisabled] = useState(true);
+  const inputHandler = ({ target: { name, value } }) => {
+    setFormInput({ ...formInput, [name]: value });
+  };
+
+  const MIN_LENGTH_PASSWORD = 5;
+  const MIN_LENGTH_NAME = 11;
+
+  useEffect(() => {
+    const validator = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    const validInput = formInput.email && formInput.password && formInput.name
+      && (validator.test(formInput.email))
+      && formInput.password.length > MIN_LENGTH_PASSWORD
+      && formInput.name.length > MIN_LENGTH_NAME;
+
+    setIsDisabled(!validInput);
+  }, [formInput]);
+
   return (
     <div>
       <form>
@@ -9,7 +29,9 @@ function Register() {
           <input
             type="text"
             id="nome"
+            name="name"
             data-testid="common_register__input-name"
+            onChange={ inputHandler }
           />
         </label>
         <label htmlFor="email">
@@ -17,7 +39,9 @@ function Register() {
           <input
             type="email"
             id="email"
+            name="email"
             data-testid="common_register__input-email"
+            onChange={ inputHandler }
           />
         </label>
         <label htmlFor="password">
@@ -25,12 +49,15 @@ function Register() {
           <input
             type="password"
             id="password"
+            name="password"
             data-testid="common_register__input-password"
+            onChange={ inputHandler }
           />
         </label>
         <button
           type="button"
           data-testid="common_register__button-register"
+          disabled={ isDisabled }
         >
           Cadastrar
         </button>
