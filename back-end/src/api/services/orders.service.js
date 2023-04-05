9a2cf5beb33fb3b665fdaecbed148aec42d0e488
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-04-04 22:57:55 -0300

    Apagando código que não estava sendo usado

--- back-end/src/api/services/orders.service.js
+++ back-end/src/api/services/orders.service.js
@@ -3,8 +3,6 @@ const GenericError = require('../utils/GenericError');
 
 const findAllOrders = async (id, next) => {
   try {
-    // const { id } = await User.findOne({ where: { email } });
-    
     const userFindAllOrders = await Sale.findAll({ where: { userId: id } });
     
     if (!userFindAllOrders) {
