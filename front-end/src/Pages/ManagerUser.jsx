affaf8164b52be657e92d8ae881dcc38ba40f504
Glaucoed <gesverzuthe@gmail.com>
2023-04-05 18:18:47 -0300

    feat: realizado requisito 37

--- front-end/src/Pages/ManagerUser.jsx
+++ front-end/src/Pages/ManagerUser.jsx
@@ -1,16 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import NavBar from '../Componentes/NavBar';
 import Context from '../Context/Context';
 
 export default function ManagerUser() {
   const { username } = useContext(Context);
-
   const [formInput, setFormInput] = useState(
-    { email: '', password: '', name: '', tipo: 'seller' },
+    { email: '', password: '', name: '', role: 'seller' },
   );
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const MIN_LENGTH_PASSWORD = 5;
+  const MIN_LENGTH_NAME = 11;
+
+  useEffect(() => {
+    const validator = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    const validInput = formInput.email && formInput.password && formInput.name
+      && (validator.test(formInput.email))
+      && formInput.password.length > MIN_LENGTH_PASSWORD
+      && formInput.name.length > MIN_LENGTH_NAME;
+
+    setIsDisabled(!validInput);
+  }, [formInput]);
 
   const inputHandler = ({ target: { name, value } }) => {
     setFormInput({ ...formInput, [name]: value });
+  };
+
+  const cadastrarUser = async (event) => {
+    event.preventDefault();
   };
 
   return (
@@ -52,12 +70,13 @@ export default function ManagerUser() {
         <label htmlFor="select">
           <span>Tipo</span>
           <select
-            name="tipo"
+            defaultValue="seller"
+            name="role"
             id="select"
             data-testid="admin_manage__select-role"
             onChange={ inputHandler }
           >
-            <option selected value="seller">Vendedor</option>
+            <option value="seller">Vendedor</option>
             <option value="customer">Cliente</option>
             <option value="administrator">Administrator</option>
 
@@ -66,6 +85,8 @@ export default function ManagerUser() {
         <button
           type="button"
           data-testid="admin_manage__button-register"
+          onClick={ (event) => cadastrarUser(event) }
+          disabled={ isDisabled }
         >
           Cadastrar
         </button>
