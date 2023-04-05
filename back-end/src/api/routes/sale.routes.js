7c46b8d126b1ae848dbc8d4e3496c58bd2ea6e15
arthutepio <arthurodriguesr10@gmail.com>
2023-04-05 19:58:55 -0300

    fix: corrige endpoint para pessoa vendedora

--- back-end/src/api/routes/sale.routes.js
+++ back-end/src/api/routes/sale.routes.js
@@ -5,7 +5,9 @@ const saleController = require('../controllers/sale.controller');
 const saleRoutes = express.Router();
 
 saleRoutes.get('/sellers', saleController.findAllSeller);
+
 saleRoutes.post('/createsale', saleController.createSale);
+
 saleRoutes.get('/sale/:id', saleController.getSaleById);
 
 module.exports = saleRoutes;
