aa81e03d2aa788c05a90405f8e0fff9fc9fd5e97
arthutepio <arthurodriguesr10@gmail.com>
2023-04-17 18:51:58 -0300

    fix: faz alterações no config.js

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.MYSQLHOST || process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQLPORT || '3306',
   database:
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+    `${process.env.MYSQLDATABASE || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
