087812d5f510bd8ee0c94616ceae4e560c556297
arthutepio <arthurodriguesr10@gmail.com>
2023-04-17 18:17:49 -0300

    preparando para deploy

--- back-end/src/api/controllers/login.controller.js
+++ back-end/src/api/controllers/login.controller.js
@@ -4,7 +4,7 @@ const loginRequest = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await loginService.loginRequest({ email, password });
-  
+
     return res.status(200).json(user);
   } catch (error) {
     next(error);
